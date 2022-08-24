dfa4322d110d2e5e79f6f4da8bd62139471337f1
MatthiasWanner <matthias.wanner@gmail.com>
2022-08-24 17:29:35 +0200

    :wrench: (config) Notion client and databases table infos as constants

---
+++ src/api/constants/notion.constants.ts
@@ -0,0 +1,12 @@
+export enum pagesProperties {
+  NAME = 'Nom',
+  TYPE = 'Type'
+}
+
+export enum pagesTypes {
+  HOME = 'Home',
+  PAGE = 'Page',
+  MENU = 'Menu',
+  ARTICLES = 'Articles',
+  PRODUCTS = 'Produits'
+}
